994beda1a1439107a7dde96efcd42c1b9a19372a
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-13 23:28:20 +0530

    login form layout creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, Center, Input, Stack} from "@chakra-ui/react"
 
 function App() {
   return (
-    
+      <Center h="100vh">
+          <Stack border="2px" borderColor="aliceblue" borderRadius={35} alignItems="center" p="4%"  bgColor="azure" spacing={5}>
+          <Input placeholder='User Name' variant="flushed" borderBlockEndColor="grey"/>
+          <Input placeholder='Password' variant="flushed" borderBlockEndColor="grey" type="password"/> 
+          <Button type="submit">Login</Button>
+          </Stack>
+      </Center>
+   
   );
 }
 
