d5e7197bd6527088114a2ea13c8c3dfcf24d1b3f
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-16 00:12:58 +0530

    created central authentication state using context APi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 import Authentication from "./Authentication";
 import Dashboard from "./Dashboard";
-import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
+
+const AuthContext=createContext();
+const useAuthContext=()=>{
+  return useContext(AuthContext)
+}
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
-  const navigate = useNavigate();
-  const navigateToDashboard = () => {
-    if (authenticated)
-      navigate('/dashboard')
-  }
+
   return (
     <>
-      {!authenticated
-        ? <Authentication />
+    <AuthContext.Provider value={{authenticated,setAuthenticated}}>
+    {!authenticated
+        ? <Authentication/>
         : (<Routes>
           <Route path="/" element={<Authentication />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
         </Routes>)
       }
+    </AuthContext.Provider>
     </>
   );
 }
 
+export {useAuthContext};
 export default App;
