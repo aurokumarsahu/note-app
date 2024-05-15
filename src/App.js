b255e3fcff5367b33b0472e221902be36844c28d
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-15 23:53:59 +0530

    added react router for routing between auth and dashboard, also made dashboard protected so that it cannot be accessed until authenticated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import Authentication from "./Authentication";
 import Dashboard from "./Dashboard";
+import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
-  const [authenticated,setAuthenticated]=useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
+  const navigateToDashboard = () => {
+    if (authenticated)
+      navigate('/dashboard')
+  }
   return (
     <>
-      <Authentication/>
-      {authenticated && <Dashboard />}
+      {!authenticated
+        ? <Authentication />
+        : (<Routes>
+          <Route path="/" element={<Authentication />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>)
+      }
     </>
   );
 }
