8dfb9da24a7dc30a5ac3364545b41296007b3451
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-18 00:44:07 +0530

    auth page delay time to 2s

--- src/Authentication.js
+++ src/Authentication.js
@@ -25,7 +25,7 @@ const Authentication=()=>{
       if(res.data==="Logged in successfully"){
         setTimeout(()=>{setAuthenticated(true);
           setUserGlobalName(cred.username);
-        },5000)
+        },2000)
         
       }
     }
