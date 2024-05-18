ee1769b6c1b665840cbb48e50fcae432e4bbd88d
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-18 17:20:34 +0530

    Logout insied Avatar menu

---
+++ src/AvatarMenu.js
@@ -0,0 +1,18 @@
+import { Menu, MenuButton, Avatar, MenuList, Button } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import Logout from "./Logout";
+
+const AvatarMenu = ({ username }) => {
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon boxSize="2rem"/>} size="fitContent" 
+             borderRadius={50} iconSpacing={-1.5} bgColor={"teal"} _hover _active color={"white"}>
+                <Avatar name={username} size="md" ></Avatar>
+            </MenuButton>
+            <MenuList padding={"6%"} w={"2%"}>
+                <Logout/>
+            </MenuList>
+        </Menu>
+    );
+}
+export default AvatarMenu;
