5e6ac4dc70292b71c1bb594608b8090abbb09c5d
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-18 12:22:53 +0530

    icon changes in login page

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,14 +1,15 @@
-import { Button, Center, HStack, Input, Spinner, Stack, Text } from "@chakra-ui/react"
-import {useState } from "react";
+import { Button, Center, Circle, HStack, Input, Spinner, Stack, Text } from "@chakra-ui/react"
+import { CheckCircleIcon,CloseIcon} from "@chakra-ui/icons"
+import { useState } from "react";
 import axios from "axios";
 import { useAuthContext } from "./App";
 
-const Authentication=()=>{
+const Authentication = () => {
   const [cred, setCred] = useState({ username: "", password: "" });
   const [isRegistered, setIsRegistered] = useState(false)
   const [loginStatus, setLoginStatus] = useState("")
   const [statusLoading, setStatusLoading] = useState(false)
-  const {setAuthenticated,setUserGlobalData}=useAuthContext()
+  const { setAuthenticated, setUserGlobalData } = useAuthContext()
 
   const handleCred = (e) => {
     setCred({ ...cred, [e.target.name]: e.target.value })
@@ -21,12 +22,13 @@ const Authentication=()=>{
       const res = await axios.post("https://my-api-kappa-flax.vercel.app/login", { ...cred })
       setStatusLoading(false)
       setLoginStatus(res.data)
-      console.log(typeof(res.data));
-      if(res.data==="Logged in successfully"){
-        setTimeout(()=>{setAuthenticated(true);
-          setUserGlobalData({...cred});
-        },300)
-        
+      console.log(typeof (res.data));
+      if (res.data === "Logged in successfully") {
+        setTimeout(() => {
+          setAuthenticated(true);
+          setUserGlobalData({ ...cred });
+        }, 300)
+
       }
     }
     else {
@@ -36,39 +38,43 @@ const Authentication=()=>{
     }
   }
 
-  
+
   return (
     <Center h="98vh">
       <form onSubmit={handleLogin}>
         <Stack border="2px" borderColor="aliceblue" borderRadius={35}
-          alignItems="center" p="6%"  bgColor="azure" 
+          alignItems="center" p="6%" bgColor="azure"
           boxShadow="lg"
           width="40vh"
           h={"40vh"}
           justifyContent={"center"}>
 
           {loginStatus.length > 0
-            ?(<HStack color={loginStatus.includes("successfully") ? "green" : "red"} fontWeight={"bolder"}>
-              {loginStatus.includes("success") ? <Text fontSize={"2rem"}>✅</Text> : '❌'+loginStatus}
-            </HStack>):
-            statusLoading && <Spinner color="teal" thickness="3px"/>
+            ?(<HStack color={loginStatus.includes("successfully") ? "green" : "red"} fontWeight={"bolder"} alignItems={"center"}>
+              {loginStatus.includes("success")
+                ?<Text fontSize={"2rem"}><CheckCircleIcon /></Text> 
+                : <Circle size="2rem" bgColor={"red"} color={"white"}><CloseIcon/></Circle>}
+                <Text>{loginStatus}</Text>
+             </HStack>
+            ) 
+            :statusLoading && <Spinner color="teal" thickness="3px" />
           }
 
-          <Input placeholder='User Name' value={cred.username} name="username" 
-          variant="flushed" borderBlockEndColor="grey" onChange={handleCred} autoComplete={"off"} required/>
-          <Input placeholder='Password' value={cred.password} name="password" 
-          variant="flushed" borderBlockEndColor="grey" type="password" onChange={handleCred} required/>
+          <Input placeholder='User Name' value={cred.username} name="username"
+            variant="flushed" borderBlockEndColor="grey" onChange={handleCred} autoComplete={"off"} required />
+          <Input placeholder='Password' value={cred.password} name="password"
+            variant="flushed" borderBlockEndColor="grey" type="password" onChange={handleCred} required />
 
           <Button type="submit" bgColor="lightseagreen" color={"white"} _hover={{ bgColor: "teal" }} _active={{ bgColor: "teal" }}>
             {isRegistered ? 'Login' : 'Register'}
-            </Button>
+          </Button>
 
-          {!isRegistered && 
-          <Button bgColor={"azure"} 
-          onClick={() => {setIsRegistered(true);setLoginStatus("");setCred({username:"",password:""})}} 
-          fontSize={".8rem"} _hover={{ fontWeight: "bolder" }} _active>
-            Already registered, I want to Login
-          </Button>}
+          {!isRegistered &&
+            <Button bgColor={"azure"}
+              onClick={() => { setIsRegistered(true); setLoginStatus(""); setCred({ username: "", password: "" }) }}
+              fontSize={".8rem"} _hover={{ fontWeight: "bolder" }} _active>
+              Already registered, I want to Login
+            </Button>}
 
         </Stack>
       </form>
