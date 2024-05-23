aa052e0dfaf8e4915390ed9a2fe6431176f224a3
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-23 23:30:23 +0530

    modified dashboard to fit Tiles

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,8 @@
-import { Box, Center, HStack, Stack, Heading, Textarea, Text, Avatar, TagRightIcon, Skeleton } from "@chakra-ui/react";
+import { Box, Center, HStack, Stack, Heading, Textarea, Text, Avatar, TagRightIcon, Skeleton, SimpleGrid } from "@chakra-ui/react";
 import { useAuthContext } from "./App";
 import Logout from "./Logout";
 import AvatarMenu from "./AvatarMenu";
+import Tiles from "./Tiles";
 
 const Dashboard = () => {
     const { userGlobalData, authenticated } = useAuthContext();
@@ -10,7 +11,7 @@ const Dashboard = () => {
             {/* Header*/}
             <Center h={"9vh"} bgColor={"teal"} color={"white"} >
                 <HStack justifyContent="space-between" w={"92vw"}>
-                    <Text fontSize={"2rem"} fontFamily={"cursive"} >WriteSpace</Text>
+                    <Text fontSize={"2rem"} fontFamily="monospace" >TaskTrack</Text>
                     <HStack justifyContent="space-around">
                         <AvatarMenu username={sessionStorage.getItem("username")} />
                         <Text>Hi, {sessionStorage.getItem("username")}</Text>
@@ -21,6 +22,11 @@ const Dashboard = () => {
 
             <HStack h={"92vh"} bgColor={"azure"} spacing={0} alignItems={"start"} justifyContent={"center"}>
                 <Box m={"1%"} w={"92vw"} >
+                    <SimpleGrid spacing={3} columns={{sm:2,md:3}}>
+                    {[1,2,3,4,5,6].map((x,inx)=>{
+                        return <Tiles key={x} Id={x}/>
+                    })}
+                    </SimpleGrid>
                     <Stack>
                         <Skeleton height='20px'  bgColor={"teal"}/>
                         <Skeleton height='20px' color={"teal"}/>
