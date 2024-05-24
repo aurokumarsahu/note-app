57c14046e7807e00fd88120e065031b70fed317e
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-24 23:57:01 +0530

    added dynamic task tiles

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,13 +3,16 @@ import { useAuthContext } from "./App";
 import Logout from "./Logout";
 import AvatarMenu from "./AvatarMenu";
 import Tiles from "./Tiles";
+import AddTask from "./AddTask";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
     const { userGlobalData, authenticated } = useAuthContext();
+    const [tasks, setTasks] = useState([])
     return (
         <>
             {/* Header*/}
-            <Center h={"9vh"}  color={"white"} position="sticky" top={0} zIndex={1} bgColor="#004C4C" >
+            <Center h={"9vh"} color={"white"} position="sticky" top={0} zIndex={1} bgColor="#004C4C" >
                 <HStack justifyContent="space-between" w={"92vw"}>
                     <Text fontSize={"2rem"} fontFamily="monospace" >TaskTrack</Text>
                     <HStack justifyContent="space-around">
@@ -22,17 +25,14 @@ const Dashboard = () => {
 
             <HStack h={"92vh"} bgColor={"azure"} spacing={0} alignItems={"start"} justifyContent={"center"}>
                 <Box m={"1%"} w={"92vw"} >
-                    <SimpleGrid spacing={3} columns={{sm:2,md:3}}>
-                    {[1,2,3,4,].map((x,inx)=>{
-                        return <Tiles key={x} Id={x}/>
-                    })}
+                    <SimpleGrid spacing={3} columns={{ sm: 2, md: 3 }}>
+                        <AddTask setTasks={setTasks}/>
+                        {
+                            tasks.map((task,inx)=>{
+                                return <Tiles key={(inx+1)*10} Id={inx+1} desc={task.taskDescription}/>
+                            })
+                        }
                     </SimpleGrid>
-                    <Stack mt={2}>
-                        <Skeleton height='20px'  bgColor={"teal"}/>
-                        <Skeleton height='20px' color={"teal"}/>
-                        <Skeleton height='20px' color={"teal"}/>
-                    </Stack>
-                    <Text color={"grey"} as={"b"}>Development in progress</Text>
                 </Box>
             </HStack>
         </>
