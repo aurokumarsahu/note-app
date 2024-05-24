ad130a750f4cbe52a65567cc839896bfe0074db9
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-25 00:03:38 +0530

    fixed add button size

--- src/AddTask.js
+++ src/AddTask.js
@@ -4,10 +4,8 @@ import {
     HStack, Icon, Text, Textarea
 } from "@chakra-ui/react";
 import { IoPersonAdd } from "react-icons/io5";
-import { IoMdAdd } from "react-icons/io";
-import EditableControls from "./EditableControls";
-import { color } from "framer-motion";
 import { useRef } from "react";
+import { AddIcon } from "@chakra-ui/icons";
 
 const AddTask = ({setTasks}) => {
     const createTaskTile=()=>{
@@ -21,13 +19,11 @@ const AddTask = ({setTasks}) => {
             <CardHeader>
                 <Box alignItems={"flex-start"}>
                     <Text fontWeight={"bold"} display={"inline"}>New Task</Text>
-                    <Button bgColor={"inherit"} _hover={{bgColor:"inherit"}} _active={{bgColor:"inherit"}} 
-                    float={"right"} p={0} onClick={createTaskTile}>
-                        <Icon as={IoMdAdd} boxSize={"2.5rem"} color={"white"}/>
-                        </Button>
+                    <AddIcon bgColor={"inherit"} _hover={{bgColor:"inherit"}} _active={{bgColor:"inherit"}} 
+                    float={"right"} p={0} onClick={createTaskTile} boxSize={"1.5rem"}/>
                 </Box>
             </CardHeader>
-            <CardBody pt={0}>
+            <CardBody>
                 <Box>
                     <Textarea resize="none" _focus={{border:"3px white solid"}} focusBorderColor="white" ref={textAreaRef} />
                 </Box>
