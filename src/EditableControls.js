e5788a8a1c67cdee266d60f3ba6d55606849b3fd
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-24 21:51:28 +0530

    Editbal Controls

---
+++ src/EditableControls.js
@@ -0,0 +1,20 @@
+import { useEditableControls,ButtonGroup,IconButton,Flex } from "@chakra-ui/react"
+export default function EditableControls() {
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls()
+
+    return isEditing ? (
+      <ButtonGroup justifyContent='center' size='sm'>
+        <IconButton icon={<CheckIcon />} {...getSubmitButtonProps()} />
+        <IconButton icon={<CloseIcon />} {...getCancelButtonProps()} />
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent='center'>
+        <IconButton size='sm' icon={<EditIcon />} {...getEditButtonProps()} />
+      </Flex>
+    )
+  }
