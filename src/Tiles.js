090542cb831940b82f2eebec78e8b2a0a8f5e188
Auro Kumar Sahu <abcdauro@outlook.com>
2024-05-24 23:56:47 +0530

    Add edit button

--- src/Tiles.js
+++ src/Tiles.js
@@ -1,17 +1,22 @@
-import { ArrowLeftIcon } from "@chakra-ui/icons";
-import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Icon,Text,Box,Textarea } from "@chakra-ui/react";
+import { EditIcon,CheckIcon } from "@chakra-ui/icons";
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Icon,Text,Box,Textarea, IconButton } from "@chakra-ui/react";
+import { color } from "framer-motion";
+import { useState } from "react";
 import { IoPersonAdd } from "react-icons/io5";
-const Tiles=({Id})=>{
+const Tiles=({Id,desc})=>{
+    const [editable,setEditable]=useState(false)
     return(
         <Card bgColor={"teal"} color={"white"}>
             <CardHeader>
-                <Text fontWeight={"bold"}>
+                <Text fontWeight={"bold"} display={"inline"}>
                     Task Id: {Id}
                 </Text>
+                <Icon as={!editable?EditIcon:CheckIcon} display={"inline"} float={"right"} boxSize={"1.5rem"} onClick={()=>setEditable(!editable)}/>
             </CardHeader>
             <CardBody pt={0} mt={4}>
                 <Box>
-                    <Textarea resize="none" _focus={{border:"3px white solid"}} focusBorderColor="white" />
+                    <Textarea resize="none" value={desc} isDisabled={!editable} _disabled={{color:"white"}}
+                    _focus={{border:"3px white solid"}} focusBorderColor="white" />
                 </Box>
             </CardBody>
             <Divider/>
